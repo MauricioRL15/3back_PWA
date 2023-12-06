5079d9f1212460c8b9ce6cf37038e19bc8903740
MauricioRL15 <15rlmauricio@gmail.com>
2023-12-06 16:11:49 -0600

    poder hacer compras

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-// app.use(express.json());
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir acceso desde cualquier origen
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos HTTP permitidos
